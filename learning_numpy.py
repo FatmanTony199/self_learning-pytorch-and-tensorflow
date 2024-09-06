703b5fb6a8fdf0cdef9148dcc625b483b42aaeaf
FatmanTony199 <tony1030127@gmail.com>
2024-09-06 14:22:53 +0800

    np Four arithmetic operations

--- learning_numpy.py
+++ learning_numpy.py
@@ -10,9 +10,21 @@ import numpy as np
 # print(np.zeros(10))
 # print(np.ones(50))
 #=====================================
-array_1 = np.array([1, 2, 3])
-
-print(array_1[0])
-print(array_1[1:3])
-mask = (array_1 % 3 == 0)
-print(array_1[mask])
+# array_1 = np.array([1, 2, 3])
+# print(array_1[0])
+# print(array_1[1:3])
+# mask = (array_1 % 3 == 0)
+# print(array_1[mask])
+#=====================================
+A = np.array([2, 4, 6])
+print("A:", A)
+B = np.array([4, 5, 6])
+print("B:", B)
+result_1 = A + B
+print("A + B:", result_1)
+result_2 = A - B
+print("A - B:",result_2)
+result_3 = A * B
+print("A * B:",result_3)
+result_4 = A / B
+print("A / B:",result_4)
