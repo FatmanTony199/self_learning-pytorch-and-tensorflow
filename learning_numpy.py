609b0a6cea4f1ea4aec2eb18dfe8fe70d3defcd6
FatmanTony199 <tony1030127@gmail.com>
2024-09-06 14:03:39 +0800

    np 2D array

--- learning_numpy.py
+++ learning_numpy.py
@@ -1,6 +1,8 @@
 #python learning_numpy.py
 import numpy as np
 
-e_list = [45, 55, 65, 78, 88]
+e_list = [[45, 55, 65, 78, 88],
+			[5, 75, 46, 53, 11],
+			[4, 5, 6, 7, 8]]
 e_array = np.array(e_list)
 print(e_array)
