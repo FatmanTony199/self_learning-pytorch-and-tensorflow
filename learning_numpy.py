b2aa6bcc1b6c9a89f9e1eea997ad7993fed68aa1
FatmanTony199 <tony1030127@gmail.com>
2024-09-06 14:11:55 +0800

    np index and mask

--- learning_numpy.py
+++ learning_numpy.py
@@ -7,5 +7,12 @@ import numpy as np
 # e_array = np.array(e_list)
 # print(e_array)
 #=====================================
-print(np.zeros(10))
-print(np.ones(50))
+# print(np.zeros(10))
+# print(np.ones(50))
+#=====================================
+array_1 = np.array([1, 2, 3])
+
+print(array_1[0])
+print(array_1[1:3])
+mask = (array_1 % 3 == 0)
+print(array_1[mask])
