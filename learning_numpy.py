77719398c57d1d8b191a555c2251507fff8127f2
FatmanTony199 <tony1030127@gmail.com>
2024-09-20 14:25:53 +0800

    np_concatenate

--- learning_numpy.py
+++ learning_numpy.py
@@ -120,4 +120,9 @@ for x in np.nditer(arr):
 
 arr = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
 for idx, x in np.ndenumerate(arr):
-  print(idx, x)
+	print(idx, x)
+
+arr1 = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
+arr2 = np.array([[11, 22, 33, 44], [55, 66, 77, 88]])
+arr = np.concatenate((arr1, arr2), axis=1)
+print(arr)
