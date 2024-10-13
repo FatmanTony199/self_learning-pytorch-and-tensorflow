c62c23b047e8a3fd35fd4f5d78be22daa88e9e0e
FatmanTony199 <tony1030127@gmail.com>
2024-10-13 16:20:19 +0800

    get_value

--- learning_sklearn.py
+++ learning_sklearn.py
@@ -3,3 +3,6 @@ import pandas as pd
 titanic_url = "https://kaggle-getting-started.s3-ap-northeast-1.amazonaws.com/titanic/train.csv"
 titanic_df = pd.read_csv(titanic_url)
 titanic_df.head()
+
+y = titanic_df[['Survived']].values.reshape(-1, 1)
+print(y)
