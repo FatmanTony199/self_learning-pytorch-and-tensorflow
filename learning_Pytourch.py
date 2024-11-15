3abc511dae62d050aebdc897fe5a27a39278df0e
FatmanTony199 <tony1030127@gmail.com>
2024-11-15 13:50:26 +0800

    tensor_gpu

--- learning_Pytourch.py
+++ learning_Pytourch.py
@@ -11,3 +11,7 @@ print(a)
 ##########################################
 a = torch.ones([2, 2])
 print(a)
+#########################################
+if torch.cuda.is_available():
+	cuda0 = torch.device('cuda', 1)
+	t1 = torch.tensor([[1, 2], [3, 4], [5, 6]], dtype=torch.float64, device=cuda0)
