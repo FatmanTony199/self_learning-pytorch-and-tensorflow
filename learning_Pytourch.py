4e78de14b87d86148e57d836d16ed123adc11dc2
FatmanTony199 <tony1030127@gmail.com>
2024-11-15 14:04:21 +0800

    tensor_num_to_ten_1

--- learning_Pytourch.py
+++ learning_Pytourch.py
@@ -16,5 +16,10 @@ if torch.cuda.is_available():
 	cuda0 = torch.device('cuda', 1)
 	t1 = torch.tensor([[1, 2], [3, 4], [5, 6]], dtype=torch.float64, device=cuda0)
 	numpy1 = t1.numpy()
-	print(“numpy1:”, numpy1)
-	print(“type:”, type(numpy1))
+	print("numpy1:", numpy1)
+	print("type:", type(numpy1))
+#######################################
+numpy2 = np.array([[1, 2, 3], [4, 5, 6]])
+
+tensor1 = torch.tensor(numpy2)
+print("dtype:", tensor1.dtype)
